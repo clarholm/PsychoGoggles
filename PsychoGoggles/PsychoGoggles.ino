75b3960872aca7c6899d8afcdbe734750dde109c
clarholm <jenslabs@gmail.com>
2014-08-21 23:04:59 +0200

    mode switch

--- PsychoGoggles/PsychoGoggles.ino
+++ PsychoGoggles/PsychoGoggles.ino
@@ -24,16 +24,16 @@ void setup() {
 
 void loop() {
   // Some example procedures showing how to display to the pixels:
-  //colorWipe(strip.Color(255, 0, 0), 50); // Red
+  colorWipe(strip.Color(0, 255, 0), 100000); // Red
   //colorWipe(strip.Color(0, 255, 0), 50); // Green
   //colorWipe(strip.Color(0, 0, 255), 50); // Blue
   // Send a theater pixel chase in...
   //theaterChase(strip.Color(127, 127, 127), 50); // White
   //theaterChase(strip.Color(127,   0,   0), 50); // Red
   //theaterChase(strip.Color(  0,   0, 127), 50); // Blue
-
-  //rainbow(20);
-  rainbowCycle(20);
+  
+  //rainbow(9000);
+  //rainbowCycle(2000);
   //theaterChaseRainbow(50);
 }
 
